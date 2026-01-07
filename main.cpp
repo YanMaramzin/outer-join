2cfda2116cad76a4bf3a52cbd891e9303b257592
yan <yan.maramzin@mail.ru>
2026-01-07 22:26:48 +0300

    [*] main.cpp configuration [*] fix main CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+#include <boost/asio.hpp>
 #include <iostream>
+#include "tcpserver.h"
 
-int main() {
-    std::cout << "Hello, Otus!";	
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "usage: join_server <port>\n";
+        return 1;
+    }
+
+    boost::asio::io_context io;
+    Server server(io, static_cast<unsigned short>(std::stoi(argv[1])));
+    io.run();
 }
-
